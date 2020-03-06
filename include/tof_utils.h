f2d94bf49c0f0c5f6ccb08bb799197cf19618d44
Georg Schramm <georg.schramm@kuleuven.be>
2020-03-06 17:03:34 +0100

    commit missing header file for AS error function approx

--- include/tof_utils.h
+++ include/tof_utils.h
@@ -1,21 +1,7 @@
 #ifndef TOF_UTILS_H
 #define TOFUTILS_H
 
-float tof_weight(float x_m0, 
-		 float x_m1, 
-		 float x_m2, 
-		 float x_v0, 
-		 float x_v1, 
-		 float x_v2, 
-		 float u0,
-		 float u1,
-		 float u2,
-		 int it,
-		 float tofbin_width,
-		 float tofcenter_offset,
-		 float sigma_tof,
-     float *erf_lut);
-
+float erff_as(float x);
 
 void relevant_tof_bins(float x_m0,
 		       float x_m1, 
