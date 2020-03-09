23315bffea8a9d8ad059e25b41db151dd5b6a993
Georg Schramm (VSC) <georg.schramm@kuleuven.be>
2020-03-09 09:53:57 +0100

    add abramowitz approx of erff as cuda function. however, they seem to be slower than the build-in erff cuda implementation

--- include/tof_utils_cuda.h
+++ include/tof_utils_cuda.h
@@ -1,21 +1,7 @@
 #ifndef __TOF_UTILS_CUDA_H__
 #define __TOF_UTILS_CUDA_H__
 
-extern "C" __device__ float tof_weight_cuda(float x_m0, 
-		                             float x_m1, 
-		                             float x_m2, 
-		                             float x_v0, 
-		                             float x_v1, 
-		                             float x_v2, 
-		                             float u0,
-		                             float u1,
-		                             float u2,
-		                             int it,
-		                             float tofbin_width,
-		                             float tofcenter_offset,
-		                             float sigma_tof,
-                                 float *erf_lut);
-
+extern "C" __device__ float erff_as_cuda(float x);
 
 extern "C" __device__ void relevant_tof_bins_cuda(float x_m0,
 		                                   float x_m1, 
