0fd63ba6c7561339b1073efcd38e3bbce5c8de4e
Georg Schramm <georg.schramm@kuleuven.be>
2020-03-12 18:10:23 +0100

    improve plot of benachmark results

--- results/plot_results.py
+++ results/plot_results.py
@@ -2,22 +2,20 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as py
 
-df = pd.read_csv('tof_sino.txt', delim_whitespace = True)
+df = pd.read_csv('genius_lm.txt', delim_whitespace = True)
 df['time/size'] = df['time']/df['size']
 
 df_fwd  = df.loc[df.dir == 'fwd']
 df_back = df.loc[df.dir == 'back']
 
 fig, ax = py.subplots(2,1, figsize = (14,8))
-sns.barplot('size', 'time/size', hue='processor',data=df_fwd,  ax = ax[0])
-sns.barplot('size', 'time/size', hue='processor',data=df_back, ax = ax[1])
+sns.swarmplot('size', 'time/size', hue='processor',data=df_fwd,  ax = ax[0], dodge = True)
+sns.swarmplot('size', 'time/size', hue='processor',data=df_back, ax = ax[1], dodge = True)
 ax[0].grid(ls = ':')
 ax[1].grid(ls = ':')
 ax[0].set_title('fwd projection')
 ax[1].set_title('back projection')
+ax[0].set_ylim(0,df_fwd['time/size'].max())
+ax[1].set_ylim(0,df_back['time/size'].max())
 fig.tight_layout()
 fig.show()
-
-
-
-py.show()
