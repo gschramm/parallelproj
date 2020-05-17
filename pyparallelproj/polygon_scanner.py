a7365e081d8f9098f207cc9a3ed9f3829ef7ce73
Georg Schramm <georg.schramm@posteo.net>
2020-05-17 19:16:48 +0200

    add function to calcuate michelogram

--- pyparallelproj/polygon_scanner.py
+++ pyparallelproj/polygon_scanner.py
@@ -1,38 +1,63 @@
 import numpy as np
-import matplotlib.pyplot as py
+from michelogram import get_michelogram_index
 
 R                   = 325
 crystals_per_module = np.array([16,9])
-crystal_size        = np.array([4,4])
-nmodules            = 28
+crystal_size        = np.array([4.,5.])
+nmodules            = np.array([28,5])
+module_gap_axial    = 5.
 
+# the distance of a crystal to the center of the module
 d = (np.arange(crystals_per_module[0]) - crystals_per_module[0]/2 + 0.5)*crystal_size[0]
 
-# crystal coordinates of one ring (single layers of crystals
-ncrystals_per_plane = nmodules*crystals_per_module[0]
+# x0 and x1 crystal coordinates of one ring (single layer of crystals)
+ncrystals_per_plane = nmodules[0]*crystals_per_module[0]
 xc0 = np.zeros(ncrystals_per_plane)
 xc1 = np.zeros(ncrystals_per_plane)
 
-for i,alpha in enumerate(np.linspace(0,2*np.pi,nmodules+1)[:-1]):
+for i,alpha in enumerate(np.linspace(0,2*np.pi,nmodules[0]+1)[:-1]):
   xc0[i*crystals_per_module[0]:(i+1)*crystals_per_module[0]] = R*np.cos(alpha) - d*np.sin(alpha)
   xc1[i*crystals_per_module[0]:(i+1)*crystals_per_module[0]] = R*np.sin(alpha) + d*np.cos(alpha)
 
+xc2 = np.zeros(crystals_per_module[1]*nmodules[1])
+
+for i in range(nmodules[1]):
+  xc2[i*crystals_per_module[1]:(i+1)*crystals_per_module[1]] = (
+      np.arange(crystals_per_module[1])*crystal_size[1] + 
+      i*(crystals_per_module[1]*crystal_size[1] + module_gap_axial))
+
+# shift center in x2 direction to 0
+xc2 -= 0.5*xc2.max()
 
 #-----------------------------------------------------------------------------------------------
-fig,ax = py.subplots(3,5, figsize = (15,9))
+# michelogram test
+naxial      = crystals_per_module[1]*nmodules[1]
+n0, n1, seg = get_michelogram_index(naxial,0)
 
-for k, view in enumerate(np.arange(15)*(224//15)):
-  istart = np.concatenate((np.repeat(np.arange(ncrystals_per_plane//2 - 1),2), 
-                           [ncrystals_per_plane//2 -1])) - view
-  iend   = np.concatenate(([-1],np.repeat(-np.arange(ncrystals_per_plane//2 - 1) - 2,2))) - view
-  
-  x0start = xc0[istart[45:-45]]
-  x1start = xc1[istart[45:-45]]
-  x0end   = xc0[iend[45:-45]]
-  x1end   = xc1[iend[45:-45]]
-  
-  for i in range(x0start.shape[0]):
-    ax.flatten()[k].plot([x0start[i], x0end[i]], [x1start[i], x1end[i]], color = 'b', lw = 0.3)
+m = np.zeros((naxial,naxial), dtype = np.int)
+s = np.zeros((naxial,naxial), dtype = np.int)
+for i in range(naxial**2):
+  m[n0[i],n1[i]] = i
+  s[n0[i],n1[i]] = seg[i]
 
-fig.tight_layout()
-fig.show()
+
+#-----------------------------------------------------------------------------------------------
+# import matplotlib.pyplot as py
+#fig,ax = py.subplots(3,5, figsize = (15,9))
+#
+#for k, view in enumerate(np.arange(15)*(224//15)):
+#  istart = np.concatenate((np.repeat(np.arange(ncrystals_per_plane//2 - 1),2), 
+#                           [ncrystals_per_plane//2 -1])) - view
+#  iend   = np.concatenate(([-1],np.repeat(-np.arange(ncrystals_per_plane//2 - 1) - 2,2))) - view
+#  
+#  xstart0 = xc0[istart[45:-45]]
+#  xstart1 = xc1[istart[45:-45]]
+#  xend0   = xc0[iend[45:-45]]
+#  xend1   = xc1[iend[45:-45]]
+#  
+#  for i in range(xstart0.shape[0]):
+#    ax.flatten()[k].plot([xstart0[i], xend0[i]], [xstart1[i], xend1[i]], color = 'b', lw = 0.1)
+#    ax.flatten()[k].set_aspect('equal')
+#
+#fig.tight_layout()
+#fig.show()
