3a613a19fbd27a83d612a9c4074e72c089c7f76c
Georg Schramm <georg.schramm@posteo.net>
2020-05-19 09:46:18 +0200

    correct radial sampling in sinogram (zig zag)

--- pyparallelproj/sinogram.py
+++ pyparallelproj/sinogram.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class PETSinogram:
 
-  def __init__(self, scanner, data = None, span = 1, rtrim = 30):
+  def __init__(self, scanner, data = None, span = 1, rtrim = 46):
     if span != 1:
       raise ValueError(f'Span {span} not supported yet')
     else:
@@ -11,8 +11,8 @@ class PETSinogram:
       self.span    = span
       self.rtrim   = rtrim
  
-      self.nrad    = scanner.ncrystals_per_plane//2 - 2*self.rtrim
-      self.nviews  = scanner.ncrystals_per_plane
+      self.nrad    = (scanner.ncrystals_per_plane + 1) - 2*self.rtrim
+      self.nviews  = scanner.ncrystals_per_plane//2
       self.nplanes = scanner.ncrystals_axial**2
 
       self.nLORs_per_view = self.nrad*self.nplanes 
@@ -43,11 +43,11 @@ class PETSinogram:
     i_tr_start = np.zeros((self.nrad, views.shape[0]), dtype = int)
     i_tr_end   = np.zeros((self.nrad, views.shape[0]), dtype = int)
 
+    n = self.scanner.ncrystals_per_plane
+
     for i, view in enumerate(views):
-      i_tr_start[:,i] = ((np.arange(self.scanner.ncrystals_per_plane // 2) + view) % 
-                              self.scanner.ncrystals_per_plane)[self.rtrim:-self.rtrim]
-      i_tr_end[:,i]   = ((-np.arange(1, 1 + self.scanner.ncrystals_per_plane // 2) + view) % 
-                              self.scanner.ncrystals_per_plane)[self.rtrim:-self.rtrim]
+      i_tr_start[:,i] = (np.concatenate((np.repeat(np.arange(n//2),2),[n//2])) - view)[self.rtrim:-self.rtrim]
+      i_tr_end[:,i]   = (np.concatenate(([-1],np.repeat(-np.arange(n//2) - 2,2))) - view)[self.rtrim:-self.rtrim]
 
       crystal_id_start = np.array(np.meshgrid(i_tr_start.flatten(), self.istart_plane)).T.reshape(-1,2) 
       crystal_id_end   = np.array(np.meshgrid(i_tr_end.flatten(),   self.iend_plane)).T.reshape(-1,2) 
@@ -60,16 +60,17 @@ if __name__ == '__main__':
  
   import matplotlib.pyplot as py
   from pet_scanners import RegularPolygonPETScanner
-  scanner = RegularPolygonPETScanner(ncrystals_per_module = np.array([16,9]),
-                                     nmodules             = np.array([28,3]))
+  scanner = RegularPolygonPETScanner(ncrystals_per_module = np.array([16,1]),
+                                     nmodules             = np.array([28,1]))
   sino    = PETSinogram(scanner)
 
-  fig,ax = py.subplots(3,5, figsize = (15,9))
- 
-  views = np.arange(15)*(448//15)
+  views = np.arange(15) * sino.nviews // 15
+
   istart, iend = sino.get_view_crystal_indices(views)
   xstart = scanner.get_crystal_coordinates(istart).reshape((sino.nrad,views.shape[0],sino.nplanes,3))
   xend   = scanner.get_crystal_coordinates(iend).reshape((sino.nrad,views.shape[0],sino.nplanes,3))
+
+  fig,ax = py.subplots(3,5, figsize = (15,9))
 
   for k, view in enumerate(views):
     for i in range(xstart.shape[0]):
@@ -78,7 +79,7 @@ if __name__ == '__main__':
     ax.flatten()[k].set_xlim(-350,350)
     ax.flatten()[k].set_ylim(-350,350)
     ax.flatten()[k].set_title(f'view {view}')
-    ax.flatten()[k].set_aspect('equal')
+    #ax.flatten()[k].set_aspect('equal')
   
   fig.tight_layout()
   fig.show()
