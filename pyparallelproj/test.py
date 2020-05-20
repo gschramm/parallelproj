0f7c2310b342faabf48ae68557a91dc7f66a17af
Georg Schramm <georg.schramm@posteo.net>
2020-05-20 10:40:12 +0200

    clean up example

--- pyparallelproj/test.py
+++ pyparallelproj/test.py
@@ -14,26 +14,32 @@ n0      = 120
 n1      = 120
 n2      = 1
 
+nsubsets    = 3
+subset      = 2 
+
+# setup a random image
 img = np.random.rand(n0,n1,n2)
 img_origin = (-(np.array(img.shape) / 2) +  0.5) * voxsize
 
-######## nontof forward projection
-sino = PETSinogram(scanner)
-proj = SinogramProjector(scanner, sino, img.shape, nsubsets = 1, 
-                         voxsize = voxsize, img_origin = img_origin)
+######## nontof projections
+sino_params = PETSinogram(scanner)
+proj        = SinogramProjector(scanner, sino_params, img.shape, nsubsets = nsubsets, 
+                                voxsize = voxsize, img_origin = img_origin)
 
-img_fwd = proj.fwd_project(img, subset = 0)
+# setup a random sinogram
+rsino = np.random.rand(*proj.subset_sino_shapes[0])
 
-rsino = np.random.rand(*img_fwd.shape)
-back = proj.back_project(rsino, subset = 0)
+img_fwd = proj.fwd_project(img, subset = subset)
+back    = proj.back_project(rsino, subset = subset)
 
-######## tof forward projection
-tofsino = PETSinogram(scanner, ntofbins = 27, tofbin_width = 28.)
-tofproj = SinogramProjector(scanner, tofsino, img.shape, nsubsets = 1, 
-                            voxsize = voxsize, img_origin = img_origin,
-                            tof = True, sigma_tof = 60., n_sigmas = 3)
+######## tof projections
+tofsino_params = PETSinogram(scanner, ntofbins = 27, tofbin_width = 28.)
+tofproj        = SinogramProjector(scanner, tofsino_params, img.shape, nsubsets = nsubsets, 
+                                   voxsize = voxsize, img_origin = img_origin,
+                                   tof = True, sigma_tof = 60., n_sigmas = 3)
 
-img_fwd_tof = tofproj.fwd_project(img, subset = 0)
+# setup a random sinogram
+tsino = np.random.rand(*tofproj.subset_sino_shapes[0])
 
-tsino = np.random.rand(*img_fwd_tof.shape)
-back_tof = proj.back_project(tsino, subset = 0)
+img_fwd_tof = tofproj.fwd_project(img, subset = subset)
+back_tof    = proj.back_project(tsino, subset = subset)
