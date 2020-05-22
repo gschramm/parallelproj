3232abaff8bdc71912af1e87ced35348a263a008
Georg Schramm <georg.schramm@posteo.net>
2020-05-22 18:34:29 +0200

    improve listmode generation by getting rid of python lists

--- examples/sino_to_lm.py
+++ examples/sino_to_lm.py
@@ -27,14 +27,10 @@ n2      = max(1,int((scanner.xc2.max() - scanner.xc2.min()) / voxsize[2]))
 
 # setup a random image
 img = np.zeros((n0,n1,n2))
-img[(n0//4):(3*n0//4),(n1//4):(3*n1//4),:] = 0.04
+img[(n0//4):(3*n0//4),(n1//4):(3*n1//4),:] = 1.5
 img_origin = (-(np.array(img.shape) / 2) +  0.5) * voxsize
 
-########  projection
-#sino_params = ppp.PETSinogram(scanner)
-#proj        = ppp.SinogramProjector(scanner, sino_params, img.shape, nsubsets = nsubsets, 
-#                                    voxsize = voxsize, img_origin = img_origin, ngpus = ngpus)
-
+# generate sinogram parameters and the projector
 sino_params = ppp.PETSinogram(scanner, ntofbins = 27, tofbin_width = 28.)
 proj        = ppp.SinogramProjector(scanner, sino_params, img.shape, nsubsets = nsubsets, 
                                     voxsize = voxsize, img_origin = img_origin, ngpus = ngpus,
@@ -52,22 +48,20 @@ back_img = proj.back_project(noisy_sino, subset = subset)
 # each event if characterize by 5 integers: 
 # [start_crystal_id_tr, start_crystal_id_ax, end_crystal_id_tr, end_crystal_id_ax, tofbin]
 
-events = []
+events  = np.zeros((noisy_sino.sum(),5), dtype = np.int16)
+counter = 0
 
 it = np.nditer(noisy_sino, flags=['multi_index'])
+
 for x in it:
   if x > 0:
-    event      = np.zeros(5, dtype = np.int16)
-    event[0:2] = proj.istart[it.multi_index[:-1]]
-    event[2:4] = proj.iend[it.multi_index[:-1]]
+    events[counter:(counter+x),0:2] = proj.istart[it.multi_index[:-1]]
+    events[counter:(counter+x):,2:4] = proj.iend[it.multi_index[:-1]]
     # for the LM projector, the central tofbin is 0, so we have to shift
     # the tof index of the sinogram bu ntofbins // 2
-    event[4]   = it.multi_index[-1] - sino_params.ntofbins // 2
+    events[counter:(counter+x):,4]   = it.multi_index[-1] - sino_params.ntofbins // 2
 
-    t = int(x)*[event]
-    events += t
-
-events = np.array(events)
+    counter += x
 
 # shuffle the event order
 tmp = np.arange(events.shape[0])
