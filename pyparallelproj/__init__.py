2050787babd915324e0418756e6ce6d52e267d6c
Georg Schramm <georg.schramm@posteo.net>
2020-05-26 18:24:44 +0200

    add files needed yo build python package

--- pyparallelproj/__init__.py
+++ pyparallelproj/__init__.py
@@ -1,3 +1,11 @@
 from .pet_scanners import RegularPolygonPETScanner
 from .projectors   import SinogramProjector, LMProjector
 from .sinogram     import PETSinogramParameters
+
+# this is needed to get the package version at runtime
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="pyparallelproj",
+    use_scm_version={'fallback_version':'unkown'},
+    setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
+    author="Georg Schramm,",
+    author_email="georg.schramm@kuleuven.be",
+    description="CUDA and OPENMP PET projectors with python bindings",
+    license='MIT',
+    long_description_content_type="text/markdown",
+    url="https://github.com/gschramm/parallelproj",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['numpy>=1.18',
+                      'matplotlib>=3.2.1']
+)
