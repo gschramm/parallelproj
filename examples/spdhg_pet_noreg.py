97b241879189ebeef374389df52596e69ef0aea2
Georg Schramm <georg.schramm@posteo.net>
2020-12-17 13:06:17 +0100

    add attenuation to spdhg example and call fwd/back model

--- examples/spdhg_pet_noreg.py
+++ examples/spdhg_pet_noreg.py
@@ -22,6 +22,8 @@ parser.add_argument('--niter',     help = 'number of iterations',  default = 10,
 parser.add_argument('--nsubsets',  help = 'number of subsets',     default = 28,  type = int)
 parser.add_argument('--likeli',    help = 'calc logLikelihodd',    action = 'store_true')
 parser.add_argument('--scat_frac', help = 'scatter fraction',      default = 0.2, type = float)
+parser.add_argument('--fwhm_mm',   help = 'psf modeling FWHM mm',  default = 4.5, type = float)
+parser.add_argument('--fwhm_data_mm',  help = 'psf for data FWHM mm',  default = 4.5, type = float)
 args = parser.parse_args()
 
 #---------------------------------------------------------------------------------
@@ -32,6 +34,8 @@ niter     = args.niter
 nsubsets  = args.nsubsets
 track_likelihood = args.likeli
 scat_frac = args.scat_frac
+fwhm_mm      = args.fwhm_mm
+fwhm_data_mm = args.fwhm_data_mm
 
 #---------------------------------------------------------------------------------
 
@@ -47,13 +51,31 @@ n0      = 120
 n1      = 120
 n2      = max(1,int((scanner.xc2.max() - scanner.xc2.min()) / voxsize[2]))
 
+# convert fwhm from mm to pixels
+fwhm      = fwhm_mm / voxsize
+fwhm_data = fwhm_data_mm / voxsize
 
-# setup a test image to be reconstructed
+# setup a random image
 img = np.zeros((n0,n1,n2), dtype = np.float32)
 img[(n0//4):(3*n0//4),(n1//4):(3*n1//4),:] = 1
 img_origin = (-(np.array(img.shape) / 2) +  0.5) * voxsize
 
-# generate sinogram parameters and the projector
+# setup an attenuation image
+att_img = (img > 0) * 0.01 * voxsize[0]
+
+# generate nonTOF sinogram parameters and the nonTOF projector for attenuation projection
+sino_params_nt = ppp.PETSinogramParameters(scanner)
+proj_nt        = ppp.SinogramProjector(scanner, sino_params_nt, img.shape, nsubsets = nsubsets, 
+                                    voxsize = voxsize, img_origin = img_origin, ngpus = ngpus)
+sino_shape_nt  = sino_params_nt.shape
+
+attn_sino = np.zeros((nsubsets, sino_shape_nt[0], sino_shape_nt[1] // nsubsets, 
+                      sino_shape_nt[2], sino_shape_nt[3]), dtype = np.float32)
+
+for i in range(nsubsets):
+    attn_sino[i, ...] = np.exp(-proj_nt.fwd_project(att_img, subset=i))
+
+# generate TOF sinogram parameters and the TOF projector
 sino_params = ppp.PETSinogramParameters(scanner, ntofbins = 17, tofbin_width = 15.)
 proj        = ppp.SinogramProjector(scanner, sino_params, img.shape, nsubsets = nsubsets, 
                                     voxsize = voxsize, img_origin = img_origin, ngpus = ngpus,
@@ -61,9 +83,6 @@ proj        = ppp.SinogramProjector(scanner, sino_params, img.shape, nsubsets = 
 
 # allocate array for the subset sinogram
 sino_shape = sino_params.shape
-img_fwd    = np.zeros((nsubsets, sino_shape[0], sino_shape[1] // nsubsets, sino_shape[2], sino_shape[3]),
-                      dtype = np.float32)
-
 
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
@@ -73,8 +92,8 @@ img_fwd    = np.zeros((nsubsets, sino_shape[0], sino_shape[1] // nsubsets, sino_
 
 test_img = np.random.rand(*img.shape).astype(np.float32)
 for pi in range(20):
-  fwd  = proj.fwd_project(test_img, subset = 0)
-  back = proj.back_project(fwd, subset = 0)
+  fwd  = attn_sino[0, ...]*proj.fwd_project(test_img, subset = 0)
+  back = proj.back_project(attn_sino[0, ...]*fwd, subset = 0)
   norm = np.linalg.norm(back)
   
   test_img = back / norm
@@ -83,18 +102,17 @@ for pi in range(20):
 pr_norm = np.sqrt(nsubsets*norm)
 
 #---------------------------------------------------------------------------
-#---------------------------------------------------------------------------
-# simulate data
 
-# generate sensitity sinogram (product from attenuation and normalization sinogram)
-# this sinogram is usually non TOF!
-# we scale the sensitivity sinogram such that the norm of the projector is approx. 1
-sens_sino = np.full(img_fwd.shape[:-1], 1./pr_norm, dtype = np.float32)
+# generate the sensitivity sinogram
+sens_sino = np.ones((nsubsets, sino_shape_nt[0], sino_shape_nt[1] // nsubsets, 
+                      sino_shape_nt[2], sino_shape_nt[3]), dtype = np.float32) / pr_norm
+
+img_fwd    = np.zeros((nsubsets, sino_shape[0], sino_shape[1] // nsubsets, sino_shape[2], 
+                       sino_shape[3]), dtype = np.float32)
 
 # forward project the image
 for i in range(nsubsets):
-  img_fwd[i,...] = sens_sino[i,...][...,np.newaxis]*proj.fwd_project(img, subset = i)
-
+  img_fwd[i,...] = ppp.pet_fwd_model(img, proj, attn_sino, sens_sino, i, fwhm = fwhm_data)
 
 # scale sum of fwd image to counts
 if counts > 0:
@@ -103,18 +121,17 @@ if counts > 0:
   img      *= scale_fac 
 
   # contamination sinogram with scatter and randoms
-  # useful to avoid division by 0 in the ratio of data and exprected data
-  contam_sino = np.full(img_fwd.shape, (scat_frac/(1 - scat_frac))*img_fwd.mean(), dtype = np.float32)
+  contam_sino = np.full(img_fwd.shape, 0.2*img_fwd.mean(), dtype = np.float32)
   
   em_sino = np.random.poisson(img_fwd + contam_sino)
 else:
   scale_fac = 1.
 
   # contamination sinogram with scatter and randoms
-  # useful to avoid division by 0 in the ratio of data and exprected data
-  contam_sino = np.full(img_fwd.shape, (scat_frac/(1 - scat_frac))*img_fwd.mean(), dtype = np.float32)
+  contam_sino = np.full(img_fwd.shape, 0.2*img_fwd.mean(), dtype = np.float32)
 
   em_sino = img_fwd + contam_sino
+
 
 #------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------
@@ -129,21 +146,25 @@ S_i = np.zeros(img_fwd.shape, dtype = np.float32)
 
 ones_img = np.ones(img.shape, dtype = np.float32)
 for i in range(nsubsets):
-  S_i[i,...] = (gamma*rho) / (sens_sino[i,...][...,np.newaxis]*proj.fwd_project(ones_img, subset = i))
-
+  S_i[i,...] = (gamma*rho) / ppp.pet_fwd_model(ones_img, proj, attn_sino, sens_sino, i, fwhm = fwhm)
 # clip inf values
 S_i[S_i == np.inf] = S_i[S_i != np.inf].max()
 
+ones_sino = np.ones((sino_shape[0], sino_shape[1] // nsubsets, sino_shape[2], 
+                     sino_shape[3]), dtype = np.float32)
 T_i = np.zeros((nsubsets,) + img.shape, dtype = np.float32)
 for i in range(nsubsets):
-  T_i[i,...] = (rho/(nsubsets*gamma)) / proj.back_project(sens_sino[i,...].repeat(proj.ntofbins).reshape(sens_sino[i,...].shape + (proj.ntofbins,)), subset = i) 
+  T_i[i,...] = (rho/(nsubsets*gamma)) / ppp.pet_back_model(ones_sino, proj, attn_sino, sens_sino, i, fwhm = fwhm)
 
 # take the element-wise min of the T_i's of all subsets
 T = T_i.min(axis = 0)
 
-# start SPDHG iterations
-x  = np.zeros(img.shape, dtype = np.float32)
 
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
+
+# start SPDHG iterations
+x      = np.zeros(img.shape, dtype = np.float32)
 z      = np.zeros(img.shape, dtype = np.float32)
 zbar   = np.zeros(img.shape, dtype = np.float32)
 y      = np.zeros(img_fwd.shape, dtype = np.float32)
@@ -151,7 +172,6 @@ y      = np.zeros(img_fwd.shape, dtype = np.float32)
 if track_likelihood:
   logL = np.zeros(niter)
 
-# start SPDHG iterations
 for iupdate in range(niter*nsubsets):
   it = iupdate // nsubsets
   ss = iupdate % nsubsets
@@ -162,12 +182,12 @@ for iupdate in range(niter*nsubsets):
   i = np.random.randint(nsubsets)
   print(f'iteration {it + 1} step {ss} subset {i}')
 
-  y_plus = y[i,...] + S_i[i,...]*(sens_sino[i,...][...,np.newaxis]*proj.fwd_project(x, subset = i) + contam_sino[i,...])
+  y_plus = y[i,...] + S_i[i,...]*ppp.pet_fwd_model(x, proj, attn_sino, sens_sino, i, fwhm = fwhm) + contam_sino[i,...]
 
   # apply the prox for the dual of the poisson logL
   y_plus = 0.5*(y_plus + 1 - np.sqrt((y_plus - 1)**2 + 4*S_i[i,...]*em_sino[i,...]))
 
-  dz = proj.back_project(sens_sino[i,...][...,np.newaxis]*(y_plus - y[i,...]), subset = i)
+  dz = ppp.pet_back_model(y_plus - y[i,...], proj, attn_sino, sens_sino, i, fwhm = fwhm)
 
   # update variables
   z = z + dz
@@ -178,8 +198,7 @@ for iupdate in range(niter*nsubsets):
   if track_likelihood and ss == (nsubsets - 1):
     exp = np.zeros(img_fwd.shape, dtype = np.float32)
     for ii in range(nsubsets):
-      exp[ii,...] = (sens_sino[ii,...][...,np.newaxis]*proj.fwd_project(x, subset = ii) + 
-                     contam_sino[ii,...])
+      exp[ii,...] = ppp.pet_fwd_model(x, proj, attn_sino, sens_sino, ii, fwhm = fwhm)+contam_sino[ii,...]
 
     logL[it] = (exp - em_sino*np.log(exp)).sum()
     print(f'neg logL {logL[it]}')
