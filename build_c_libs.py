4569c5401a856990f57503ccc831a6ff4f35bf94
Georg Schramm <georg.schramm@posteo.net>
2021-02-01 18:12:48 +0100

    add CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE to build script

--- build_c_libs.py
+++ build_c_libs.py
@@ -8,6 +8,11 @@ from tempfile import TemporaryDirectory
 build_dir  = TemporaryDirectory(prefix = 'build', dir = '.')
 source_dir = '.'
 
-os.system(f'cmake -B {build_dir.name} {source_dir}')
+if os.name == 'nt':
+  cmake_args = '-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE'
+else:
+  cmake_args = ''
+
+os.system(f'cmake -B {build_dir.name} {cmake_args} {source_dir}')
 os.system(f'cmake --build {build_dir.name}')
 os.system(f'cmake --install {build_dir.name}')
