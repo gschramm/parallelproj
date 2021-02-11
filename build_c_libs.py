23ecfdcd97fa002924ee057f350529addc27725e
Georg Schramm <georg.schramm@posteo.net>
2021-02-11 21:18:33 +0100

    add install_dir command line options in CMakeLists and adapt python build script accordingly

--- build_c_libs.py
+++ build_c_libs.py
@@ -2,16 +2,54 @@
 # to build all C and CUDA libs
 # supposed to be OS independent
 
+import argparse
 import os
-from tempfile import TemporaryDirectory
+from tempfile import mkdtemp
+from shutil import rmtree
 
-build_dir  = TemporaryDirectory(prefix = 'build', dir = '.')
-source_dir = '.'
+parser = argparse.ArgumentParser(description = 'Build C/CUDA libs with cmake and install \
+                                                them to the correct location for the python package')
+parser.add_argument('--build_dir', help = 'temp build directory', 
+                    default = None)
+parser.add_argument('--source_dir', help = 'cmake source dir', 
+                    default = os.path.dirname(os.path.abspath(__file__)))
+parser.add_argument('--install_dir', help = 'cmake INSTALL_LIB_DIR - default: %(default)s', 
+                    default = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                           'pyparallelproj','lib'))
+parser.add_argument('--target_dir', help = 'cmake INSTALL_TARGET_DIR - default: %(default)s', 
+                    default = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                           'pyparallelproj','lib'))
+parser.add_argument('--keep_build_dir', help = 'do not remove tempory build dir', 
+                    action = 'store_true')
+args = parser.parse_args()
+
+#---------------------------------------------------------------------------------------------
+
+if args.build_dir is None:
+  build_dir  = mkdtemp(prefix = 'build_', dir = '.')
+else:
+  build_dir = args.build_dir
+
+source_dir = args.source_dir
+
+install_dir = args.install_dir
+
+target_dir = args.target_dir
+
+remove_build_dir = not args.keep_build_dir
+
+#---------------------------------------------------------------------------------------------
 
 if os.name == 'nt':
-  os.system(f'cmake -B {build_dir.name} -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE {source_dir}')
-  os.system(f'cmake --build {build_dir.name} --target INSTALL --config RELEASE')
+  os.system(f'cmake -B {build_dir} -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DINSTALL_LIB_DIR={install_dir} -DINSTALL_TARGET_DIR={target_dir} {source_dir}')
+  os.system(f'cmake --build {build_dir} --target INSTALL --config RELEASE')
 else:
-  os.system(f'cmake -B {build_dir.name} {source_dir}')
-  os.system(f'cmake --build {build_dir.name}')
-  os.system(f'cmake --install {build_dir.name}')
+  os.system(f'cmake -B {build_dir} -DINSTALL_LIB_DIR={install_dir} -DINSTALL_TARGET_DIR={target_dir}  {source_dir}')
+  os.system(f'cmake --build {build_dir}')
+  os.system(f'cmake --install {build_dir}')
+
+if remove_build_dir:
+  rmtree(build_dir)
+else:
+  print(f'Kept build directory {build_dir}')
+
