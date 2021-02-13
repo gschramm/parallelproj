5deb80738792717b3826835936a47f22903024e2
Georg Schramm <georg.schramm@posteo.net>
2021-02-13 19:26:16 +0100

    reorganize structure of C and CUDA code and CMakeLists.txt using subdirectories

--- build_libs_for_python.py
+++ build_libs_for_python.py
@@ -13,12 +13,8 @@ parser.add_argument('--build_dir', help = 'temp build directory',
                     default = None)
 parser.add_argument('--source_dir', help = 'cmake source dir', 
                     default = os.path.dirname(os.path.abspath(__file__)))
-parser.add_argument('--install_dir', help = 'cmake INSTALL_LIB_DIR - default: %(default)s', 
-                    default = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                           'pyparallelproj','lib'))
-parser.add_argument('--target_dir', help = 'cmake CONFIG_FILE_DIR - default: %(default)s', 
-                    default = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                           'pyparallelproj','lib'))
+parser.add_argument('--cmake_install_prefix', help = 'cmake INSTALL_LIB_DIR - default: %(default)s', 
+                    default = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pyparallelproj'))
 parser.add_argument('--keep_build_dir', help = 'do not remove tempory build dir', 
                     action = 'store_true')
 parser.add_argument('--dry', help = 'dry run - only print cmake commands', 
@@ -34,9 +30,7 @@ else:
 
 source_dir = args.source_dir
 
-install_dir = args.install_dir
-
-target_dir = args.target_dir
+cmake_install_prefix = args.cmake_install_prefix
 
 remove_build_dir = not args.keep_build_dir
 
@@ -45,7 +39,7 @@ dry = args.dry
 #---------------------------------------------------------------------------------------------
 
 if os.name == 'nt':
-  cmd1 = f'cmake -B {build_dir} -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DINSTALL_LIB_DIR={install_dir} -DCONFIG_FILE_DIR={target_dir} {source_dir}'
+  cmd1 = f'cmake -B {build_dir} -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DCMAKE_INSTALL_PREFIX={cmake_install_prefix} {source_dir}'
   cmd2 = f'cmake --build {build_dir} --target INSTALL --config RELEASE'
 
   if dry:
@@ -55,7 +49,7 @@ if os.name == 'nt':
     os.system(cmd1)
     os.system(cmd2)
 else:
-  cmd1 = f'cmake -B {build_dir} -DINSTALL_LIB_DIR={install_dir} -DCONFIG_FILE_DIR={target_dir} {source_dir}'
+  cmd1 = f'cmake -B {build_dir} -DCMAKE_INSTALL_PREFIX={cmake_install_prefix} {source_dir}'
   cmd2 = f'cmake --build {build_dir}'
   cmd3 = f'cmake --install {build_dir}'
 
