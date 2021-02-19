a221306675c396188a6c0737500a785bea3d0654
Georg Schramm <georg.schramm@posteo.net>
2021-02-19 11:56:46 +0100

    add test openacc test files

---
+++ openacc/include/ray_cube_intersection.h
@@ -0,0 +1,18 @@
+#ifndef __PARALLELPROJ_RAY_CUBE_INTERSECTION_H__
+#define __PARALLELPROJ_RAY_CUBE_INTERSECTION_H__
+
+unsigned char ray_cube_intersection(float orig0,
+                                    float orig1,
+                                    float orig2,
+                                    float bounds0_min,
+                                    float bounds1_min,
+                                    float bounds2_min,
+                                    float bounds0_max,
+                                    float bounds1_max,
+                                    float bounds2_max,
+                                    float rdir0,
+                                    float rdir1,
+                                    float rdir2,
+                                    float* t1,
+                                    float* t2);
+#endif
