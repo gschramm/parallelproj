66f20882266b5bff08466a338f080993aff77893
Georg Schramm <georg.schramm@kuleuven.be>
2021-09-23 16:50:02 +0200

    update dependencies in conda recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,4 @@ CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ${CMAKE_PLATFORM_FLAGS[@]} -DCMAKE_BUILD_TYPE="Release" ${SRC_DIR}
 
-#cmake --build . --target install
-make VERBOSE=2
+cmake --build . --target install
