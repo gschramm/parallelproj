45f0100cfa0f2847e50f7ab54ba007c41fa432ae
Georg Schramm <georg.schramm@kuleuven.be>
2022-03-14 15:09:09 +0100

    remove "_2" in function names of OPENMP backprojectors

--- c/src/joseph3d_back.c
+++ c/src/joseph3d_back.c
@@ -10,14 +10,14 @@
 #include "ray_cube_intersection.h"
 
 
-void joseph3d_back_2(const float *xstart, 
-                     const float *xend, 
-                     float *img,
-                     const float *img_origin, 
-                     const float *voxsize,
-                     const float *p, 
-                     long long nlors, 
-                     const int *img_dim)
+void joseph3d_back(const float *xstart, 
+                   const float *xend, 
+                   float *img,
+                   const float *img_origin, 
+                   const float *voxsize,
+                   const float *p, 
+                   long long nlors, 
+                   const int *img_dim)
 {
   long long i;
 
--- c/src/joseph3d_back_tof_lm.c
+++ c/src/joseph3d_back_tof_lm.c
@@ -12,21 +12,21 @@
 #include "ray_cube_intersection.h"
 
 
-void joseph3d_back_tof_lm_2(const float *xstart, 
-                            const float *xend, 
-                            float *img,
-                            const float *img_origin, 
-                            const float *voxsize,
-                            const float *p, 
-                            long long nlors, 
-                            const int *img_dim,
-                            float tofbin_width,
-                            const float *sigma_tof,
-                            const float *tofcenter_offset,
-                            float n_sigmas,
-                            const short *tof_bin,
-                            unsigned char lor_dependent_sigma_tof,
-                            unsigned char lor_dependent_tofcenter_offset)
+void joseph3d_back_tof_lm(const float *xstart, 
+                          const float *xend, 
+                          float *img,
+                          const float *img_origin, 
+                          const float *voxsize,
+                          const float *p, 
+                          long long nlors, 
+                          const int *img_dim,
+                          float tofbin_width,
+                          const float *sigma_tof,
+                          const float *tofcenter_offset,
+                          float n_sigmas,
+                          const short *tof_bin,
+                          unsigned char lor_dependent_sigma_tof,
+                          unsigned char lor_dependent_tofcenter_offset)
 
 {
   long long i;
