906fdcb1f800c11ce6fca112dbdf31f00e75038b
Georg Schramm <georg.schramm@posteo.net>
2022-08-06 13:07:58 -0700

    cosmectics in lor sampling wip

--- wip/lor_sampling.py
+++ wip/lor_sampling.py
@@ -5,27 +5,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.ion()
 
-def plot_view_lors(proj, view, save = False):
+def plot_view_lors(proj, view, ax):
     """ function to plot LORs of a 1 ring cylindrical scanner """
     # get start and end coordinates of a specific view
     xstart, xend = proj.get_subset_sino_coordinates(view)
     xstart = xstart.squeeze()[:-1]
     xend = xend.squeeze()[:-1]
 
-    fig, ax = proj.scanner.show_crystal_config()
     for i in range(xstart.shape[0]):
-        ax[0].plot([xstart[i,0], xend[i,0]], [xstart[i,1], xend[i,1]], 'g')
-    ax[0].plot(xstart[:,0], xstart[:,1], 'g^')
-    ax[0].set_aspect('equal')
-
-    fig.tight_layout()
-    fig.show()
-
-    return fig, ax
+        ax.plot([xstart[i,0], xend[i,0]], [xstart[i,1], xend[i,1]], 'g')
+    ax.set_aspect('equal')
 
 #--------------------------------------------------------------------------
 
-def main():
+def main(views = [80,81,82]):
+    plt.rcParams['image.cmap'] = 'Greys'
+
     # setup a scanner
     scanner = ppp.RegularPolygonPETScanner(ncrystals_per_module=np.array([15, 1]),
                                            nmodules=np.array([12, 1]),
@@ -34,7 +29,7 @@ def main():
 
     # setup a test image
     voxsize = np.array([2.2, 2.2, 1.1])
-    n0 = int((1.8*scanner.R) / voxsize[0] )
+    n0 = int((2.1*scanner.R) / voxsize[0] )
     n1 = n0
     n2 = 1
 
@@ -57,20 +52,41 @@ def main():
     # use number of subsets equal to number of projection angles
     proj.init_subsets(proj.sino_params.nontof_shape[1])
 
-    for view in [72,73,74]:
-        fig, ax = plot_view_lors(proj, view, save = False)
-        ax[0].plot([vox[0]*voxsize[0] + img_origin[0]],
-                   [vox[1]*voxsize[1] + img_origin[1]],
-                   'b+')
-
-    ######### nontof projections
-    # setup a random sinogram
+    # forward project image
     img_fwd = proj.fwd_project(img)
+    # backporject sinogram of ones
+    back = proj.back_project(0*img_fwd + 1)
 
-    fig2, ax2 = plt.subplots()
-    ax2.imshow(img_fwd.squeeze().T)
+    fig2, ax2 = plt.subplots(1,2, figsize=(12,6))
+    ax2[0].imshow(img_fwd.squeeze().T, interpolation='none')
+    ax2[0].set_title(f'fwd proj point source')
+    ax2[1].imshow(back, interpolation='none')
+    ax2[1].set_title(f'back proj ones')
+    ax2[0].set_aspect('equal')
+    ax2[1].set_aspect('equal')
     fig2.tight_layout()
     fig2.show()
+
+
+    for view in views:
+        fig, ax = plt.subplots(1,2, figsize=(12,6))
+        plot_view_lors(proj, view, ax[0])
+        ax[0].plot([vox[0]*voxsize[0] + img_origin[0]],
+                   [vox[1]*voxsize[1] + img_origin[1]],
+                   'r+', ms = 10)
+        ax[0].set_title(f'view {view} LORs')
+        ax[0].set_xlim(-90,90)
+
+        sino = np.zeros(img_fwd.shape)
+        sino[:,view,...] = 1
+        ax[1].imshow(proj.back_project(sino).squeeze().T,
+                     interpolation='none', origin='lower')
+        ax[1].set_title(f'back proj view {view}')
+
+        ax[0].set_aspect('equal')
+        ax[1].set_aspect('equal')
+        fig.tight_layout()
+        fig.show()
 
 #--------------------------------------------------------------------------
 
