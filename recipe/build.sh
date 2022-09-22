fc89c824be9fe7369e242cf9b8948d91d6770482
Georg Schramm <georg.schramm@kuleuven.be>
2022-09-22 19:43:38 +0000

    update recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,2 +1,3 @@
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_LIBDIR=lib ${CMAKE_ARGS} -DCMAKE_CUDA_ARCHITECTURES=all ${SRC_DIR}
-cmake --build . --target install
+cmake --build . --target install --verbose
+cmake --build . --target docs --verbose
