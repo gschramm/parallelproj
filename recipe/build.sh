1a739b148c09abe93ceba4fa36ff691b8eb4e946
Georg Schramm <georg.schramm@kuleuven.be>
2022-09-26 22:37:21 +0000

    run ctest in build scripts

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,4 @@
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_LIBDIR=lib ${CMAKE_ARGS} -DCMAKE_CUDA_ARCHITECTURES=all ${SRC_DIR}
 cmake --build . --target install --verbose
 cmake --build . --target docs --verbose
+ctest -VV
