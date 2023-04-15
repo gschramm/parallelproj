1df1cc15bac1502df6de816848870f326f048c4b
Georg <gschra2@stanford.edu>
2023-04-15 16:34:44 -0700

    add adjointness test (indirect test for back projection)

--- python/test/test_nontof_joseph.py
+++ python/test/test_nontof_joseph.py
@@ -80,9 +80,77 @@ def fwd_test(xp: ModuleType, verbose=True) -> bool:
 #--------------------------------------------------------------------------
 
 
+def adjointness_test(xp: ModuleType,
+                     nLORs=1000000,
+                     seed=1,
+                     verbose=True) -> bool:
+    """test whether backprojection is the adjoint of forward projection
+       indirect test whether back projection is correct (assuming fwd projection is correct)
+    """
+    xp.random.seed(seed)
+    n0, n1, n2 = (16, 15, 17)
+
+    img_dim = xp.array([n0, n1, n2])
+    voxel_size = xp.array([0.7, 0.8, 0.6], dtype=xp.float32)
+    img_origin = ((-img_dim / 2 + 0.5) * voxel_size).astype(xp.float32)
+    img = xp.random.rand(n0, n1, n2).astype(xp.float32)
+
+    # generate random LORs on a sphere around the image volume
+    R = 0.8 * (img_dim * voxel_size).max()
+
+    phis = xp.random.rand(nLORs) * 2 * xp.pi
+    costheta = xp.random.rand(nLORs) * 2 - 1
+    sintheta = xp.sqrt(1 - costheta**2)
+
+    xstart = xp.zeros((nLORs, 3), dtype=xp.float32)
+    xstart[:, 0] = R * sintheta * xp.cos(phis)
+    xstart[:, 1] = R * sintheta * xp.sin(phis)
+    xstart[:, 2] = R * costheta
+
+    phis = xp.random.rand(nLORs) * 2 * xp.pi
+    costheta = xp.random.rand(nLORs) * 2 - 1
+    sintheta = xp.sqrt(1 - costheta**2)
+
+    xend = xp.zeros((nLORs, 3), dtype=xp.float32)
+    xend[:, 0] = R * sintheta * xp.cos(phis)
+    xend[:, 1] = R * sintheta * xp.sin(phis)
+    xend[:, 2] = R * costheta
+
+    # forward project
+    img_fwd = xp.zeros(xstart.shape[0], dtype=xp.float32)
+    parallelproj.joseph3d_fwd(xstart, xend, img, img_origin, voxel_size,
+                              img_fwd)
+
+    # backward project
+    back_img = xp.zeros_like(img)
+    sino = xp.random.rand(nLORs).astype(xp.float32)
+    parallelproj.joseph3d_back(xstart, xend, back_img, img_origin, voxel_size,
+                               sino)
+
+    ip_a = (back_img * img).sum()
+    ip_b = (img_fwd * sino).sum()
+
+    res = np.isclose(ip_a, ip_b)
+
+    if verbose:
+        print(
+            f'module = {xp.__name__}  -  cuda_enabled {parallelproj.cuda_enabled}'
+        )
+        print('ip_a = ', ip_a)
+        print('ip_b = ', ip_b)
+        print('')
+
+    return res
+
+
+#--------------------------------------------------------------------------
+
+
 class TestNonTOFJoseph(unittest.TestCase):
+    """test for non TOF joseph projections"""
 
     def test_fwd(self):
+        """test non TOF joseph forward projection using different backends"""
         self.assertTrue(fwd_test(np))
 
         if parallelproj.cupy_enabled:
@@ -92,6 +160,19 @@ class TestNonTOFJoseph(unittest.TestCase):
         if parallelproj.cuda_enabled:
             parallelproj.cuda_enabled = False
             self.assertTrue(fwd_test(np))
+            parallelproj.cuda_enabled = True
+
+    def test_adjoint(self):
+        """test non TOF joseph forward projection using different backends"""
+        self.assertTrue(adjointness_test(np))
+
+        if parallelproj.cupy_enabled:
+            import cupy as cp
+            self.assertTrue(adjointness_test(cp))
+
+        if parallelproj.cuda_enabled:
+            parallelproj.cuda_enabled = False
+            self.assertTrue(adjointness_test(np))
             parallelproj.cuda_enabled = True
 
 
