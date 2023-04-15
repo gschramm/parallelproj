04076dd0324a7b057f03e3c9bda22481e9a1454b
Georg <gschra2@stanford.edu>
2023-04-14 17:23:40 -0700

    remove recipe (moved to conda-forge feedstock)

--- recipe/bld.bat
+++
@@ -1,3 +0,0 @@
-cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_LIBDIR=lib %CMAKE_ARGS% -DCMAKE_CUDA_ARCHITECTURES=all %SRC_DIR%
-cmake --build . --target install --verbose
-ctest -VV
