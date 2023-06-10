23dfaac70873dce6b7e9e2fa003516a61b0bd0c5
Georg <gschra2@stanford.edu>
2023-06-10 14:04:58 -0700

    split python classes

--- python/parallelproj/operators.py
+++ python/parallelproj/operators.py
@@ -1,19 +1,6 @@
 from __future__ import annotations
 import abc
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
-import parallelproj
-
-
-def tonumpy(x, xp):
-    if xp.__name__ == 'numpy':
-        return x
-    elif xp.__name__ == 'cupy':
-        return xp.asnumpy(x)
-    else:
-        raise ValueError
-
 
 class LinearOperator(abc.ABC):
 
@@ -216,160 +203,36 @@ class GaussianFilterOperator(LinearOperator):
         return self._call(y)
 
 
-class ParallelViewProjector2D(LinearOperator):
-
-    def __init__(self, image_shape, radial_positions, num_views, radius,
-                 image_origin, voxel_size, xp):
-
-        super().__init__()
-
-        self._image_shape = image_shape
-        self._radial_positions = radial_positions
-
-        self._num_rad = radial_positions.shape[0]
-        self._num_views = num_views
-
-        self._radius = radius
-        self._image_origin = image_origin
-        self._voxel_size = voxel_size
-
-        self._xp = xp
-
-        # array of projection angles
-        self._view_angles = self._xp.linspace(0,
-                                              xp.pi,
-                                              self._num_views,
-                                              endpoint=False)
-
-        self._xstart = self._xp.zeros((self._num_views, self._num_rad, 3),
-                                      dtype=xp.float32)
-        self._xend = self._xp.zeros((self._num_views, self._num_rad, 3),
-                                    dtype=xp.float32)
-
-        for i, phi in enumerate(self._view_angles):
-            # world coordinates of LOR start points
-            self._xstart[
-                i, :,
-                1] = self._xp.cos(phi) * self._radial_positions + self._xp.sin(
-                    phi) * self._radius
-            self._xstart[i, :, 2] = -self._xp.sin(
-                phi) * self._radial_positions + self._xp.cos(
-                    phi) * self._radius
-            # world coordinates of LOR endpoints
-            self._xend[
-                i, :,
-                1] = self._xp.cos(phi) * self._radial_positions - self._xp.sin(
-                    phi) * self._radius
-            self._xend[i, :, 2] = -self._xp.sin(
-                phi) * self._radial_positions - self._xp.cos(
-                    phi) * self._radius
-
-    @property
-    def in_shape(self):
-        return self._image_shape
-
-    @property
-    def out_shape(self):
-        return (self._num_views, self._num_rad)
-
-    def _call(self, x):
-        y = self._xp.zeros(self.out_shape, dtype=self._xp.float32)
-        parallelproj.joseph3d_fwd(self._xstart.reshape(-1, 3),
-                                  self._xend.reshape(-1, 3),
-                                  x.astype(self._xp.float32),
-                                  self._image_origin, self._voxel_size, y)
-        return y
-
-    def _adjoint(self, y):
-        x = self._xp.zeros(self.in_shape, dtype=self._xp.float32)
-        parallelproj.joseph3d_back(self._xstart.reshape(-1, 3),
-                                   self._xend.reshape(-1, 3), x,
-                                   self._image_origin, self._voxel_size,
-                                   y.astype(self._xp.float32))
-        return x
-
-    def show_views(self, views_to_show=None, image=None, **kwargs):
-        if views_to_show is None:
-            views_to_show = np.linspace(0, self._num_views - 1, 5).astype(int)
-
-        num_cols = len(views_to_show)
-        fig, ax = plt.subplots(1, num_cols, figsize=(num_cols * 3, 3))
-
-        tmp1 = float(self._image_origin[1] - 0.5 * self._voxel_size[1])
-        tmp2 = float(self._image_origin[2] - 0.5 * self._voxel_size[2])
-        img_extent = [tmp1, -tmp1, tmp2, -tmp2]
-
-        for i, ip in enumerate(views_to_show):
-            ax[i].plot(tonumpy(self._xstart[ip, :, 1], self._xp),
-                       tonumpy(self._xstart[ip, :, 2], self._xp),
-                       '.',
-                       ms=0.5)
-            ax[i].plot(tonumpy(self._xend[ip, :, 1], self._xp),
-                       tonumpy(self._xend[ip, :, 2], self._xp),
-                       '.',
-                       ms=0.5)
-            for k in np.linspace(0, self._num_rad - 1, 7).astype(int):
-                ax[i].plot([
-                    float(self._xstart[ip, k, 1]),
-                    float(self._xend[ip, k, 1])
-                ], [
-                    float(self._xstart[ip, k, 2]),
-                    float(self._xend[ip, k, 2])
-                ],
-                           'k-',
-                           lw=0.5)
-                ax[i].annotate(f'{k}', (float(
-                    self._xstart[ip, k, 1]), float(self._xstart[ip, k, 2])),
-                               fontsize='xx-small')
-            ax[i].set_xlim(-500, 500)
-            ax[i].set_ylim(-500, 500)
-            ax[i].grid(ls=':')
-            ax[i].set_aspect('equal')
-
-            if image is not None:
-                ax[i].add_patch(
-                    Rectangle((tmp1, tmp2),
-                              float(self.in_shape[1] * self._voxel_size[1]),
-                              float(self.in_shape[2] * self._voxel_size[2]),
-                              edgecolor='r',
-                              facecolor='none',
-                              linestyle=':'))
-                ax[i].imshow(tonumpy(image[0, ...], self._xp).T,
-                             origin='lower',
-                             extent=img_extent,
-                             **kwargs)
-            ax[i].set_title(
-                f'view {ip:03} - phi {(180/np.pi)*self._view_angles[ip]:.1f} deg',
-                fontsize='small')
-
-        fig.tight_layout()
-
-        return fig
-
 
 #----------------------------------------------------
 
 if __name__ == '__main__':
-    import scipy.ndimage as ndi
+    #import numpy as xp
+    #import scipy.ndimage as ndi
+
+    import cupy as xp
+    import cupyx.scipy.ndimage as ndi
+
+    from parallelproj.projectors import ParallelViewProjector2D
 
     #----------------------------------------------------------------------
     projector = ParallelViewProjector2D((1, 128, 128),
-                                np.linspace(-200, 200, 131, dtype=np.float32),
+                                xp.linspace(-200, 200, 131, dtype=xp.float32),
                                 210, 300.,
-                                np.array([0, -63.5, -63.5], dtype=np.float32),
-                                np.ones(3, dtype=np.float32), np)
+                                xp.array([0, -63.5, -63.5], dtype=xp.float32),
+                                xp.ones(3, dtype=xp.float32), xp)
 
 
     image_space_filter = GaussianFilterOperator(projector.in_shape,
                                ndi,
                                sigma=1.3)
 
-    sensitivity = ElementwiseMultiplicationOperator(np.full(projector.out_shape, 2.3, dtype=np.float32))
+    sensitivity = ElementwiseMultiplicationOperator(xp.full(projector.out_shape, 2.3, dtype=xp.float32))
 
     fwd_model = CompositeLinearOperator((sensitivity, projector, image_space_filter))
 
-    fwd_model.adjointness_test(np, verbose=True)
-    fwd_model_norm = fwd_model.norm(np)
+    fwd_model.adjointness_test(xp, verbose=True)
+    fwd_model_norm = fwd_model.norm(xp)
 
     fig = projector.show_views()
     fig.show()
---
+++ python/parallelproj/projectors.py
@@ -0,0 +1,137 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import parallelproj
+from parallelproj.operators import LinearOperator
+from parallelproj.utils import tonumpy
+
+class ParallelViewProjector2D(LinearOperator):
+
+    def __init__(self, image_shape, radial_positions, num_views, radius,
+                 image_origin, voxel_size, xp):
+
+        super().__init__()
+
+        self._image_shape = image_shape
+        self._radial_positions = radial_positions
+
+        self._num_rad = radial_positions.shape[0]
+        self._num_views = num_views
+
+        self._radius = radius
+        self._image_origin = image_origin
+        self._voxel_size = voxel_size
+
+        self._xp = xp
+
+        # array of projection angles
+        self._view_angles = self._xp.linspace(0,
+                                              xp.pi,
+                                              self._num_views,
+                                              endpoint=False)
+
+        self._xstart = self._xp.zeros((self._num_views, self._num_rad, 3),
+                                      dtype=xp.float32)
+        self._xend = self._xp.zeros((self._num_views, self._num_rad, 3),
+                                    dtype=xp.float32)
+
+        for i, phi in enumerate(self._view_angles):
+            # world coordinates of LOR start points
+            self._xstart[
+                i, :,
+                1] = self._xp.cos(phi) * self._radial_positions + self._xp.sin(
+                    phi) * self._radius
+            self._xstart[i, :, 2] = -self._xp.sin(
+                phi) * self._radial_positions + self._xp.cos(
+                    phi) * self._radius
+            # world coordinates of LOR endpoints
+            self._xend[
+                i, :,
+                1] = self._xp.cos(phi) * self._radial_positions - self._xp.sin(
+                    phi) * self._radius
+            self._xend[i, :, 2] = -self._xp.sin(
+                phi) * self._radial_positions - self._xp.cos(
+                    phi) * self._radius
+
+    @property
+    def in_shape(self):
+        return self._image_shape
+
+    @property
+    def out_shape(self):
+        return (self._num_views, self._num_rad)
+
+    def _call(self, x):
+        y = self._xp.zeros(self.out_shape, dtype=self._xp.float32)
+        parallelproj.joseph3d_fwd(self._xstart.reshape(-1, 3),
+                                  self._xend.reshape(-1, 3),
+                                  x.astype(self._xp.float32),
+                                  self._image_origin, self._voxel_size, y)
+        return y
+
+    def _adjoint(self, y):
+        x = self._xp.zeros(self.in_shape, dtype=self._xp.float32)
+        parallelproj.joseph3d_back(self._xstart.reshape(-1, 3),
+                                   self._xend.reshape(-1, 3), x,
+                                   self._image_origin, self._voxel_size,
+                                   y.astype(self._xp.float32))
+        return x
+
+    def show_views(self, views_to_show=None, image=None, **kwargs):
+        if views_to_show is None:
+            views_to_show = np.linspace(0, self._num_views - 1, 5).astype(int)
+
+        num_cols = len(views_to_show)
+        fig, ax = plt.subplots(1, num_cols, figsize=(num_cols * 3, 3))
+
+        tmp1 = float(self._image_origin[1] - 0.5 * self._voxel_size[1])
+        tmp2 = float(self._image_origin[2] - 0.5 * self._voxel_size[2])
+        img_extent = [tmp1, -tmp1, tmp2, -tmp2]
+
+        for i, ip in enumerate(views_to_show):
+            ax[i].plot(tonumpy(self._xstart[ip, :, 1], self._xp),
+                       tonumpy(self._xstart[ip, :, 2], self._xp),
+                       '.',
+                       ms=0.5)
+            ax[i].plot(tonumpy(self._xend[ip, :, 1], self._xp),
+                       tonumpy(self._xend[ip, :, 2], self._xp),
+                       '.',
+                       ms=0.5)
+            for k in np.linspace(0, self._num_rad - 1, 7).astype(int):
+                ax[i].plot([
+                    float(self._xstart[ip, k, 1]),
+                    float(self._xend[ip, k, 1])
+                ], [
+                    float(self._xstart[ip, k, 2]),
+                    float(self._xend[ip, k, 2])
+                ],
+                           'k-',
+                           lw=0.5)
+                ax[i].annotate(f'{k}', (float(
+                    self._xstart[ip, k, 1]), float(self._xstart[ip, k, 2])),
+                               fontsize='xx-small')
+            ax[i].set_xlim(-500, 500)
+            ax[i].set_ylim(-500, 500)
+            ax[i].grid(ls=':')
+            ax[i].set_aspect('equal')
+
+            if image is not None:
+                ax[i].add_patch(
+                    Rectangle((tmp1, tmp2),
+                              float(self.in_shape[1] * self._voxel_size[1]),
+                              float(self.in_shape[2] * self._voxel_size[2]),
+                              edgecolor='r',
+                              facecolor='none',
+                              linestyle=':'))
+                ax[i].imshow(tonumpy(image[0, ...], self._xp).T,
+                             origin='lower',
+                             extent=img_extent,
+                             **kwargs)
+            ax[i].set_title(
+                f'view {ip:03} - phi {(180/np.pi)*self._view_angles[ip]:.1f} deg',
+                fontsize='small')
+
+        fig.tight_layout()
+
+        return fig
+
