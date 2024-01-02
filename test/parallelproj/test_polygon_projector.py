886f8fe75848db22df5f4d59a8c21349c5ef04b0
Georg Schramm <georg.schramm@kuleuven.be>
2024-01-02 15:54:40 +0100

    test polygon projector with all comb. of sym. axis ans sinogram order

--- test/parallelproj/test_polygon_projector.py
+++ test/parallelproj/test_polygon_projector.py
@@ -101,6 +101,16 @@ def test_polygon_projector(xp: ModuleType, dev: str) -> None:
         # should raise an error if we don't pass None | TOFParameters
         proj2.tof_parameters = 3.5
 
+def _get_slice(r, v, p, lor_descriptor):
+
+    sl = [slice(0), slice(0), slice(0)]
+
+    sl[lor_descriptor.radial_axis_num] = slice(r, r + 1, None)
+    sl[lor_descriptor.view_axis_num] = slice(v, v + 1, None)
+    sl[lor_descriptor.plane_axis_num] = slice(p, p + 1, None)
+
+    return tuple(sl)
+
 def test_minimal_reg_polygon_projector(xp, dev) -> None:
     """test forward joseph non-tof forward projection with a minimal scanner geometry
        using a 3x3x3 image and a single voxel !=0 at the center of the image
@@ -114,63 +124,65 @@ def test_minimal_reg_polygon_projector(xp, dev) -> None:
 
     img_shape = 3*(img_size,)
     
-    scanner = parallelproj.RegularPolygonPETScannerGeometry(
-        xp, dev, radius = radius, num_sides = 8,
-        num_lor_endpoints_per_side = 1, lor_spacing = 1.,
-        num_rings = 3, ring_positions = xp.asarray([-z, 0, z], device = dev),
-        symmetry_axis = 2)
-    
-    sinogram_order = parallelproj.SinogramSpatialAxisOrder.RVP
-    lor_desc = parallelproj.RegularPolygonPETLORDescriptor(scanner, radial_trim = 1, sinogram_order = sinogram_order)
-    
-    proj = parallelproj.RegularPolygonPETProjector(lor_desc, img_shape = img_shape, voxel_size = 3*(vox_size,))
-    
     # setup a test image with a single voxel != 0 at the center of the image
     x = xp.zeros(img_shape, device = dev)
     x[img_shape[0]//2, img_shape[1]//2, img_shape[2]//2] = vox_value
-    x_fwd = proj(x)
-    
-    # check "corner to corner" projection which should be vox size * vox value * sqrt(3)
-    assert np.isclose(float(x_fwd[2,0,-1]), vox_value * vox_size * np.sqrt(3))
-    assert np.isclose(float(x_fwd[2,2,-1]), vox_value * vox_size * np.sqrt(3))
-    assert np.isclose(float(x_fwd[2,0,-2]), vox_value * vox_size * np.sqrt(3))
-    assert np.isclose(float(x_fwd[2,2,-2]), vox_value * vox_size * np.sqrt(3))
-    
-    # check "central" (straight through) projection which should be vox size * vox value
-    assert np.isclose(float(x_fwd[2,1,1]), vox_value * vox_size)
-    assert np.isclose(float(x_fwd[2,3,1]), vox_value * vox_size)
-    
-    # check "corner to corner" projection which should be vox size * vox value * sqrt(2)
-    assert np.isclose(float(x_fwd[2,0,1]), vox_value * vox_size * np.sqrt(2))
-    assert np.isclose(float(x_fwd[2,2,1]), vox_value * vox_size * np.sqrt(2))
-
     # setup a test image where all voxels have the same value
     x2 = vox_value*xp.ones(img_shape, device = dev)
-    x_fwd2 = proj(x2)
 
-    # check "corner to corner" projection which should be vox size * vox value * sqrt(3)
-    assert np.isclose(float(x_fwd2[2,0,-1]), img_size * vox_value * vox_size * np.sqrt(3))
-    assert np.isclose(float(x_fwd2[2,2,-1]), img_size * vox_value * vox_size * np.sqrt(3))
-    assert np.isclose(float(x_fwd2[2,0,-2]), img_size * vox_value * vox_size * np.sqrt(3))
-    assert np.isclose(float(x_fwd2[2,2,-2]), img_size * vox_value * vox_size * np.sqrt(3))
+    for symmetry_axis in [0,1,2]:
+        scanner = parallelproj.RegularPolygonPETScannerGeometry(
+            xp, dev, radius = radius, num_sides = 8,
+            num_lor_endpoints_per_side = 1, lor_spacing = 1.,
+            num_rings = 3, ring_positions = xp.asarray([-z, 0, z], device = dev),
+            symmetry_axis = symmetry_axis)
     
-    # check "central" (straight through) projection which should be vox size * vox value
-    assert np.isclose(float(x_fwd2[2,1,1]), img_size * vox_value * vox_size)
-    assert np.isclose(float(x_fwd2[2,3,1]), img_size * vox_value * vox_size)
+        for sinogram_order in parallelproj.SinogramSpatialAxisOrder:
+            lor_desc = parallelproj.RegularPolygonPETLORDescriptor(scanner, radial_trim = 1, sinogram_order = sinogram_order)
     
-    # check "corner to corner" projection which should be vox size * vox value * sqrt(2)
-    assert np.isclose(float(x_fwd2[2,0,1]), img_size * vox_value * vox_size * np.sqrt(2))
-    assert np.isclose(float(x_fwd2[2,2,1]), img_size * vox_value * vox_size * np.sqrt(2))
+            proj = parallelproj.RegularPolygonPETProjector(lor_desc, img_shape = img_shape, voxel_size = 3*(vox_size,))
+    
+            x_fwd = proj(x)
+    
+            # check "corner to corner" projection which should be vox size * vox value * sqrt(3)
+            assert np.isclose(float(x_fwd[_get_slice(2,0,lor_desc.num_planes-1, lor_desc)][0,0,0]), vox_value * vox_size * np.sqrt(3))
+            assert np.isclose(float(x_fwd[_get_slice(2,2,lor_desc.num_planes-1, lor_desc)][0,0,0]), vox_value * vox_size * np.sqrt(3))
+            assert np.isclose(float(x_fwd[_get_slice(2,0,lor_desc.num_planes-2, lor_desc)][0,0,0]), vox_value * vox_size * np.sqrt(3))
+            assert np.isclose(float(x_fwd[_get_slice(2,2,lor_desc.num_planes-2, lor_desc)][0,0,0]), vox_value * vox_size * np.sqrt(3))
+    
+            ## check "central" (straight through) projection which should be vox size * vox value
+            assert np.isclose(float(x_fwd[_get_slice(2,1,1, lor_desc)][0,0,0]), vox_value * vox_size)
+            assert np.isclose(float(x_fwd[_get_slice(2,3,1, lor_desc)][0,0,0]), vox_value * vox_size)
+    
+            # check "corner to corner" projection which should be vox size * vox value * sqrt(2)
+            assert np.isclose(float(x_fwd[_get_slice(2,0,1, lor_desc)][0,0,0]), vox_value * vox_size * np.sqrt(2))
+            assert np.isclose(float(x_fwd[_get_slice(2,2,1, lor_desc)][0,0,0]), vox_value * vox_size * np.sqrt(2))
 
-    # setup the same projector without caching of the LOR endpoints
-    projb = parallelproj.RegularPolygonPETProjector(lor_desc, img_shape = img_shape, voxel_size = 3*(vox_size,), cache_lor_endpoints = False)
-    x_fwd2b = projb(x2)
+            x_fwd2 = proj(x2)
 
-    projb.adjointness_test(xp, dev)
+            # check "corner to corner" projection which should be vox size * vox value * sqrt(3)
+            assert np.isclose(float(x_fwd2[_get_slice(2,0,lor_desc.num_planes-1, lor_desc)][0,0,0]), img_size * vox_value * vox_size * np.sqrt(3))
+            assert np.isclose(float(x_fwd2[_get_slice(2,2,lor_desc.num_planes-1, lor_desc)][0,0,0]), img_size * vox_value * vox_size * np.sqrt(3))
+            assert np.isclose(float(x_fwd2[_get_slice(2,0,lor_desc.num_planes-2, lor_desc)][0,0,0]), img_size * vox_value * vox_size * np.sqrt(3))
+            assert np.isclose(float(x_fwd2[_get_slice(2,2,lor_desc.num_planes-2, lor_desc)][0,0,0]), img_size * vox_value * vox_size * np.sqrt(3))
+    
+            ## check "central" (straight through) projection which should be vox size * vox value
+            assert np.isclose(float(x_fwd2[_get_slice(2,1,1, lor_desc)][0,0,0]), img_size * vox_value * vox_size)
+            assert np.isclose(float(x_fwd2[_get_slice(2,3,1, lor_desc)][0,0,0]), img_size * vox_value * vox_size)
+    
+            # check "corner to corner" projection which should be vox size * vox value * sqrt(2)
+            assert np.isclose(float(x_fwd2[_get_slice(2,0,1, lor_desc)][0,0,0]), img_size * vox_value * vox_size * np.sqrt(2))
+            assert np.isclose(float(x_fwd2[_get_slice(2,2,1, lor_desc)][0,0,0]), img_size * vox_value * vox_size * np.sqrt(2))
 
-    assert projb.xstart is None
-    assert projb.xend is None
-    assert projb.lor_descriptor == lor_desc
+            # setup the same projector without caching of the LOR endpoints
+            projb = parallelproj.RegularPolygonPETProjector(lor_desc, img_shape = img_shape, voxel_size = 3*(vox_size,), cache_lor_endpoints = False)
+            x_fwd2b = projb(x2)
 
-    # check whether the projections with and without caching the LOR endpoints are the same
-    assert np.allclose(np.asarray(to_device(x_fwd2b, 'cpu')), np.asarray(to_device(x_fwd2, 'cpu')))
+            projb.adjointness_test(xp, dev)
+
+            assert projb.xstart is None
+            assert projb.xend is None
+            assert projb.lor_descriptor == lor_desc
+
+            # check whether the projections with and without caching the LOR endpoints are the same
+            assert np.allclose(np.asarray(to_device(x_fwd2b, 'cpu')), np.asarray(to_device(x_fwd2, 'cpu')))
