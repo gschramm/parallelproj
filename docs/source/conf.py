9d5e62f9b5b8ab371f402fd64df3a631ff871e8a
Georg Schramm <georg.schramm@kuleuven.be>
2024-01-08 09:59:08 +0100

    add MLEM example

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,52 +5,54 @@
 
 import os
 import sys
+
 # add parallelproj to the PYTHONPATH
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'python')))
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "python")))
 
 import json
+from sphinx_gallery.sorting import FileNameSortKey
 
 # get version string from file
-with open(os.path.join('..', '..', 'package.json')) as f:
-    version = json.load(f)['version']
+with open(os.path.join("..", "..", "package.json")) as f:
+    version = json.load(f)["version"]
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'parallelproj'
-copyright = '2023, Georg Schramm'
-author = 'Georg Schramm'
+project = "parallelproj"
+copyright = "2023, Georg Schramm"
+author = "Georg Schramm"
 release = version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.coverage',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.mathjax', 
-    'sphinx.ext.viewcode',
-    'sphinx_gallery.gen_gallery',
-    'sphinx_design',
-    'sphinx_copybutton',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx_gallery.gen_gallery",
+    "sphinx_design",
+    "sphinx_copybutton",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
 
 sphinx_gallery_conf = {
     "examples_dirs": ["../../examples"],
-    #"gallery_dirs": "auto_examples",  # path to where to save gallery generated output
+    # "gallery_dirs": "auto_examples",  # path to where to save gallery generated output
     "filename_pattern": "/demo_",
     "run_stale_examples": True,
     "ignore_pattern": r"__init__\.py",
@@ -67,38 +69,45 @@ sphinx_gallery_conf = {
     # is an empty set, i.e. exclude nothing.
     "exclude_implicit_doc": {},
     "nested_sections": False,
+    "within_subsection_order": FileNameSortKey,
+    "line_numbers": True,
+    "recommender": {"enable": True},
+    "first_notebook_cell": (
+        "# This cell is added by sphinx-gallery\n"
+        "# It can be customized to whatever you like\n"
+    ),
 }
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_logo = 'parllelproj-logo2.svg'
+html_theme = "sphinx_rtd_theme"
+html_logo = "parllelproj-logo2.svg"
 
 html_theme_options = {
-    'logo_only': True,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
+    "logo_only": True,
+    "display_version": True,
+    "prev_next_buttons_location": "bottom",
+    "style_external_links": False,
+    "vcs_pageview_mode": "",
     #'style_nav_header_background': '#1e8449',
     # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False,
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "includehidden": True,
+    "titles_only": False,
 }
 
-#html_context = {
+# html_context = {
 #  'display_github': True,
 #  'github_user': 'gschramm',
 #  'github_repo': 'parallelproj',
 #  'github_version': 'master/docs/source/',
-#}
+# }
 
 # -- Options for EPUB output -------------------------------------------------
-epub_show_urls = 'footnote'
+epub_show_urls = "footnote"
 
 # -- napoleon options --------------------------------------------------------
 napoleon_google_docstring = False
@@ -114,7 +123,7 @@ autodoc_default_options = {
     "show-inheritance": True,
 }
 
-autoclass_content = 'both'
+autoclass_content = "both"
 
 # -- coverage builder options -------------------------------------------------
 # Configuration of sphinx.ext.coverage
@@ -123,20 +132,22 @@ coverage_statistics_to_stdout = True
 
 # -- nbsphinx options ---------------------------------------------------------
 nbsphinx_custom_formats = {
-    '.pct.py': ['jupytext.reads', {'fmt': 'py:percent'}],
+    ".pct.py": ["jupytext.reads", {"fmt": "py:percent"}],
 }
 
 # -- print warnings for undocumented things ------------------------------------
 # https://stackoverflow.com/questions/14141170/how-can-i-just-list-undocumented-members-with-sphinx-autodoc
 # set up the types of member to check that are documented
-members_to_watch = ['function','class','method','attribute','property']
+members_to_watch = ["function", "class", "method", "attribute", "property"]
+
 
 def warn_undocumented_members(app, what, name, obj, options, lines):
-    if(what in members_to_watch and len(lines)==0):
+    if what in members_to_watch and len(lines) == 0:
         # warn to terminal during build
-        print("WARNING: ", what, "is undocumented: ", name, "(%d)"% len(lines))
+        print("WARNING: ", what, "is undocumented: ", name, "(%d)" % len(lines))
         # or modify the docstring so the rendered output is highlights the omission
         lines.append(".. Warning:: %s '%s' undocumented" % (what, name))
 
+
 def setup(app):
-    app.connect('autodoc-process-docstring', warn_undocumented_members)
+    app.connect("autodoc-process-docstring", warn_undocumented_members)
--- examples/05_algorithms/demo_01_mlem.py
+++ examples/05_algorithms/demo_01_mlem.py
@@ -1,8 +1,16 @@
 """
-MLEM
-====
+Basic MLEM
+==========
 
-foo bar
+This example demonstrates the use of the MLEM algorithm to minimize the negative Poisson log-likelihood function.
+
+.. math::
+    f(x) = \sum_{i=1}^m \\bar{y}_i - \\bar{y}_i (x) \log(y_i)
+
+using the linear forward model
+
+.. math::
+    \\bar{y}(x) = A x + s
 """
 # %%
 # parallelproj supports the numpy, cupy and pytorch array API and different devices
@@ -35,11 +43,15 @@ elif "torch" in xp.__name__:
 
 # %%
 
-A = xp.asarray([[1, 4, 2], [3, 2, 5], [0, 1, 4]], dtype=xp.float64, device=dev)
+A = xp.asarray(
+    [[2.5, 1.2, 0, 0], [0, 3.1, 0.7, 0], [0, 0, 4.1, 2.5], [0.2, 0, 0, 0.9]],
+    dtype=xp.float64,
+    device=dev,
+)
 
 op = parallelproj.MatrixOperator(A)
 
-x_true = xp.asarray([5.5, 10.7, 8.2], dtype=xp.float64, device=dev)
+x_true = xp.asarray([5.5, 10.7, 8.2, 7.9], dtype=xp.float64, device=dev)
 
 noise_free_data = op(x_true)
 
@@ -52,7 +64,7 @@ noisy_data = xp.asarray(
 
 # noisy_data = xp.floor(noise_free_data)
 
-contamination = xp.asarray([0.1, 0.1, 0.1], dtype=xp.float64, device=dev)
+contamination = xp.asarray([0.3, 0.2, 0.1, 0.4], dtype=xp.float64, device=dev)
 
 # %%
 
@@ -63,27 +75,33 @@ x_ref = A_inv @ (noisy_data - contamination)
 exp_ref = op(x_ref) + contamination
 cost_ref = float(xp.sum(exp_ref - noisy_data * xp.log(exp_ref)))
 # %%
-num_iter = 1000
+num_iter = 500
 
 x = xp.ones(op.in_shape, dtype=xp.float64, device=dev)
 ones_back = op.adjoint(xp.ones(op.out_shape, dtype=xp.float64, device=dev))
 
-cost = xp.zeros(num_iter, dtype=xp.float64, device=dev)
-dist = xp.zeros(num_iter, dtype=xp.float64, device=dev)
+rel_cost = xp.zeros(num_iter, dtype=xp.float64, device=dev)
+rel_dist = xp.zeros(num_iter, dtype=xp.float64, device=dev)
 
 for i in range(num_iter):
     exp = op(x) + contamination
-    cost[i] = xp.sum(exp - noisy_data * xp.log(exp))
-    dist[i] = xp.linalg.vector_norm(x - x_ref) / xp.linalg.vector_norm(x_ref)
+    rel_cost[i] = (xp.sum(exp - noisy_data * xp.log(exp)) - cost_ref) / abs(cost_ref)
+    rel_dist[i] = xp.linalg.vector_norm(x - x_ref) / xp.linalg.vector_norm(x_ref)
     ratio = noisy_data / exp
     x *= op.adjoint(ratio) / ones_back
 
 
 # %%
 
-fig, ax = plt.subplots(1, 2, figsize=(8, 4))
-ax[0].plot(np.asarray(to_device(cost, "cpu")))
-ax[1].loglog(np.asarray(to_device(dist, "cpu")))
-ax[0].set_ylim(cost_ref - 0.1 * (float(cost[1]) - cost_ref), float(cost[2]))
+fig, ax = plt.subplots(1, 2, figsize=(8, 4), sharex=True)
+ax[0].semilogx(np.asarray(to_device(rel_cost, "cpu")))
+ax[1].loglog(np.asarray(to_device(rel_dist, "cpu")))
+ax[0].set_ylim(-rel_cost[2], rel_cost[2])
+ax[0].set_ylabel(r"( f($x$) - f($x^*$) )   /   | f($x^*$) |")
+ax[1].set_ylabel(r"rel. distance to optimum $\|x - x^*\| / \|x^*\|$")
+ax[0].set_xlabel("iteration")
+ax[1].set_xlabel("iteration")
+ax[0].grid(ls=":")
+ax[1].grid(ls=":")
 fig.tight_layout()
 fig.show()
