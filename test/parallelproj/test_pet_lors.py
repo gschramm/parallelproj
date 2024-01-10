a0dc2ef30e84bdfd89b3976b6962c47ced7897eb
Georg Schramm <georg.schramm@kuleuven.be>
2024-01-10 10:46:16 +0100

    add views to subset distribution test

--- test/parallelproj/test_pet_lors.py
+++ test/parallelproj/test_pet_lors.py
@@ -50,6 +50,95 @@ def test_pet_lors(xp: ModuleType, dev: str) -> None:
         plt.close(fig)
 
     # test lor descriptor without max_ring_difference and radial_trim
-    lor_desc2 = parallelproj.RegularPolygonPETLORDescriptor(
-        scanner, radial_trim=radial_trim, max_ring_difference=None
+    scanner2 = parallelproj.RegularPolygonPETScannerGeometry(
+        xp,
+        dev,
+        radius=65.0,
+        num_sides=12,
+        num_lor_endpoints_per_side=15,
+        lor_spacing=2.3,
+        ring_positions=xp.linspace(-10, 10, num_rings),
+        symmetry_axis=2,
     )
+
+    num_subsets = 9
+
+    # test the distribution of views into subsets
+    for sinogram_order in parallelproj.SinogramSpatialAxisOrder:
+        lor_desc2 = parallelproj.RegularPolygonPETLORDescriptor(
+            scanner2,
+            radial_trim=10,
+            max_ring_difference=None,
+            sinogram_order=sinogram_order,
+        )
+
+        for num_dim in [3, 4]:
+            subset_views, subset_slices = lor_desc2.get_distributed_views_and_slices(
+                num_subsets, num_dim
+            )
+
+            assert bool(
+                xp.all(
+                    xp.sort(xp.concat(subset_views))
+                    == xp.arange(lor_desc2.num_views, device=dev)
+                )
+            )
+
+            assert bool(
+                xp.all(
+                    subset_views[0]
+                    == xp.asarray([0, 9, 18, 27, 36, 45, 54, 63, 72, 81], device=dev)
+                )
+            )
+            assert bool(
+                xp.all(
+                    subset_views[1]
+                    == xp.asarray([4, 13, 22, 31, 40, 49, 58, 67, 76, 85], device=dev)
+                )
+            )
+            assert bool(
+                xp.all(
+                    subset_views[2]
+                    == xp.asarray([8, 17, 26, 35, 44, 53, 62, 71, 80, 89], device=dev)
+                )
+            )
+            assert bool(
+                xp.all(
+                    subset_views[3]
+                    == xp.asarray([1, 10, 19, 28, 37, 46, 55, 64, 73, 82], device=dev)
+                )
+            )
+            assert bool(
+                xp.all(
+                    subset_views[4]
+                    == xp.asarray([5, 14, 23, 32, 41, 50, 59, 68, 77, 86], device=dev)
+                )
+            )
+            assert bool(
+                xp.all(
+                    subset_views[5]
+                    == xp.asarray([2, 11, 20, 29, 38, 47, 56, 65, 74, 83], device=dev)
+                )
+            )
+            assert bool(
+                xp.all(
+                    subset_views[6]
+                    == xp.asarray([6, 15, 24, 33, 42, 51, 60, 69, 78, 87], device=dev)
+                )
+            )
+            assert bool(
+                xp.all(
+                    subset_views[7]
+                    == xp.asarray([3, 12, 21, 30, 39, 48, 57, 66, 75, 84], device=dev)
+                )
+            )
+            assert bool(
+                xp.all(
+                    subset_views[8]
+                    == xp.asarray([7, 16, 25, 34, 43, 52, 61, 70, 79, 88], device=dev)
+                )
+            )
+
+            sl = num_dim * [slice(None)]
+            sl[lor_desc2.view_axis_num] = slice(0, None, num_subsets)
+            assert subset_slices[0] == tuple(sl)
