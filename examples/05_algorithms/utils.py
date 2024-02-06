385c63408d0b60713215c3bdb1d086ec64d85196
Georg Schramm <40211162+gschramm@users.noreply.github.com>
2024-02-06 18:35:30 +0100

    Radon square (#58)
    * add radon square
    
    * correct bug in scaling of RT
    
    * bugfix in shifting
    
    * add rotation to radon object

--- examples/05_algorithms/utils.py
+++ examples/05_algorithms/utils.py
@@ -17,6 +17,7 @@ class RadonObject(abc.ABC):
         self._s0: float = 1.0
         self._s1: float = 1.0
         self._amplitude: float = 1.0
+        self._rotation: float = 0.0
 
     @abc.abstractmethod
     def _centered_radon_transform(self, r: Array, phi: Array) -> Array:
@@ -74,20 +75,33 @@ class RadonObject(abc.ABC):
     def amplitude(self, value: float) -> None:
         self._amplitude = value
 
+    @property
+    def rotation(self) -> float:
+        return self._rotation
+
+    @rotation.setter
+    def rotation(self, value: float) -> None:
+        self._rotation = value
+
     def radon_transform(self, s, phi) -> float:
+
+        phi_rotated = (phi + self.rotation) % (2 * self.xp.pi)
+
         s_prime = s / self.xp.sqrt(
-            self._s0**2 * self.xp.cos(phi) ** 2 + self._s1**2 * self.xp.sin(phi) ** 2
+            self._s0**2 * self.xp.cos(phi_rotated) ** 2
+            + self._s1**2 * self.xp.sin(phi_rotated) ** 2
         )
         phi_prime = self.xp.atan2(
-            self._s0 * self.xp.sin(phi), self._s1 * self.xp.cos(phi)
+            self._s0 * self.xp.cos(phi_rotated),
+            self._s1 * self.xp.sin(phi_rotated),
         )
 
         fac = (
             self._s0
             * self._s1
             / self.xp.sqrt(
-                self._s0**2 * self.xp.cos(phi) ** 2
-                + self._s1**2 * self.xp.sin(phi) ** 2
+                self._s0**2 * self.xp.cos(phi_rotated) ** 2
+                + self._s1**2 * self.xp.sin(phi_rotated) ** 2
             )
         )
 
@@ -96,16 +110,20 @@ class RadonObject(abc.ABC):
             * fac
             * self._centered_radon_transform(
                 s_prime
-                - self._x0_offset * self.xp.cos(phi_prime)
-                - self._x1_offset * self.xp.sin(phi_prime),
+                - self._x0_offset * self.xp.sin(phi_prime)
+                - self._x1_offset * self.xp.cos(phi_prime),
                 phi_prime,
             )
         )
 
     def values(self, x0: Array, x1: Array) -> Array:
-        return self._amplitude * self._centered_values(
-            x0 / self._s0 - self._x0_offset, x1 / self._s1 - self._x1_offset
-        )
+        x0_p = x0 * self.xp.cos(self._rotation) - x1 * self.xp.sin(self._rotation)
+        x1_p = x0 * self.xp.sin(self._rotation) + x1 * self.xp.cos(self._rotation)
+
+        x0_pp = x0_p / self._s0 - self._x0_offset
+        x1_pp = x1_p / self._s1 - self._x1_offset
+
+        return self._amplitude * self._centered_values(x0_pp, x1_pp)
 
 
 class RadonObjectSequence(Sequence[RadonObject]):
@@ -155,3 +173,79 @@ class RadonDisk(RadonObject):
     @radius.setter
     def radius(self, value: float) -> None:
         self._radius = value
+
+
+class RadonSquare(RadonObject):
+    """2D square with known radon transform"""
+
+    def __init__(self, xp: ModuleType, dev: str, width: float) -> None:
+        super().__init__(xp, dev)
+        self._width: float = width
+        self._eps = 1e-6
+
+    def _centered_radon_transform(self, r: Array, phi: Array) -> Array:
+        # calculate the angle alpha which is "distance to pi/4"
+        f1 = phi // (self.xp.pi / 4)
+        f2 = f1 % 2
+        m = -(2 * f2 - 1)
+        alpha = m * (phi - f1 * self.xp.pi / 4) + f2 * self.xp.pi / 4
+
+        abs_r = self.xp.abs(r)
+
+        # height of the plateau
+        h = 2 * self.a / self.xp.cos(alpha)
+
+        sqrt2 = float(self.xp.sqrt(self.xp.asarray(2.0)))
+
+        # r2 is the distance until we have the plateau
+        r2 = sqrt2 * self.a * self.xp.cos(self.xp.pi / 4 + alpha)
+        # between r2 and r1 we have the triangle part
+        r1 = sqrt2 * self.a * self.xp.cos(self.xp.pi / 4 - alpha)
+
+        mask1 = self.xp.where(
+            (r1 - r2) > self.eps, self.xp.ones_like(r), self.xp.zeros_like(r)
+        )
+        mask2 = self.xp.where(abs_r >= r2, self.xp.ones_like(r), self.xp.zeros_like(r))
+        mask3 = self.xp.where(abs_r < r1, self.xp.ones_like(r), self.xp.zeros_like(r))
+
+        # triangle mask
+        t_mask = mask1 * mask2 * mask3
+
+        # plateau aprt
+        p1 = self.xp.where(abs_r < r2, h * self.xp.ones_like(r), self.xp.zeros_like(r))
+
+        # triangle part avoiding division by zero
+        denom = self.xp.where(
+            r1 - r2 > self.eps, r1 - r2, self.eps * self.xp.ones_like(r)
+        )
+        tmp = -self.xp.sign(r) * h * self.xp.divide(r - self.xp.sign(r) * r2, denom) + h
+        p2 = self.xp.where(t_mask == 1, tmp, self.xp.zeros_like(r))
+
+        return p1 + p2
+
+    def _centered_values(self, x0: Array, x1: Array) -> Array:
+        return self.xp.where(
+            self.xp.logical_and(self.xp.abs(x0) <= self.a, self.xp.abs(x1) <= self.a),
+            self.xp.ones_like(x0),
+            self.xp.zeros_like(x0),
+        )
+
+    @property
+    def width(self) -> float:
+        return self._width
+
+    @width.setter
+    def radius(self, value: float) -> None:
+        self._radius = value
+
+    @property
+    def a(self) -> float:
+        return self._width / 2
+
+    @property
+    def eps(self) -> float:
+        return self._eps
+
+    @eps.setter
+    def eps(self, value: float) -> None:
+        self._eps = value
