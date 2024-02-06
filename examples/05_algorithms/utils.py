6d7d2eac8d000b14657e37b405963b1281845d33
Georg Schramm <georg.schramm@kuleuven.be>
2024-02-06 15:29:20 +0100

    add radon square

--- examples/05_algorithms/utils.py
+++ examples/05_algorithms/utils.py
@@ -155,3 +155,79 @@ class RadonDisk(RadonObject):
     @radius.setter
     def radius(self, value: float) -> None:
         self._radius = value
+
+
+class RadonSquare(RadonObject):
+    """2D square with known radon transform"""
+
+    def __init__(self, xp: ModuleType, dev: str, width: float) -> None:
+        super().__init__(xp, dev)
+        self._width: float = width
+        self._eps = 1e-6
+
+    def _centered_radon_transform(self, r: Array, phi: Array) -> Array:
+        # calculate the angle alpha which is "distance to pi/4"
+        f1 = phi // (self.xp.pi / 4)
+        f2 = f1 % 2
+        m = -(2 * f2 - 1)
+        alpha = m * (phi - f1 * self.xp.pi / 4) + f2 * self.xp.pi / 4
+
+        abs_r = self.xp.abs(r)
+
+        # height of the plateau
+        h = 2 * self.a / self.xp.cos(alpha)
+
+        sqrt2 = float(self.xp.sqrt(self.xp.asarray(2.0)))
+
+        # r2 is the distance until we have the plateau
+        r2 = sqrt2 * self.a * self.xp.cos(self.xp.pi / 4 + alpha)
+        # between r2 and r1 we have the triangle part
+        r1 = sqrt2 * self.a * self.xp.cos(self.xp.pi / 4 - alpha)
+
+        mask1 = self.xp.where(
+            (r1 - r2) > self.eps, self.xp.ones_like(r), self.xp.zeros_like(r)
+        )
+        mask2 = self.xp.where(abs_r >= r2, self.xp.ones_like(r), self.xp.zeros_like(r))
+        mask3 = self.xp.where(abs_r < r1, self.xp.ones_like(r), self.xp.zeros_like(r))
+
+        # triangle mask
+        t_mask = mask1 * mask2 * mask3
+
+        # plateau aprt
+        p1 = self.xp.where(abs_r < r2, h * self.xp.ones_like(r), self.xp.zeros_like(r))
+
+        # triangle part avoiding division by zero
+        denom = self.xp.where(
+            r1 - r2 > self.eps, r1 - r2, self.eps * self.xp.ones_like(r)
+        )
+        tmp = -self.xp.sign(r) * h * self.xp.divide(r - self.xp.sign(r) * r2, denom) + h
+        p2 = self.xp.where(t_mask == 1, tmp, self.xp.zeros_like(r))
+
+        return p1 + p2
+
+    def _centered_values(self, x0: Array, x1: Array) -> Array:
+        return self.xp.where(
+            self.xp.logical_and(self.xp.abs(x0) <= self.a, self.xp.abs(x1) <= self.a),
+            self.xp.ones_like(x0),
+            self.xp.zeros_like(x0),
+        )
+
+    @property
+    def width(self) -> float:
+        return self._width
+
+    @width.setter
+    def radius(self, value: float) -> None:
+        self._radius = value
+
+    @property
+    def a(self) -> float:
+        return self._width / 2
+
+    @property
+    def eps(self) -> float:
+        return self._eps
+
+    @eps.setter
+    def eps(self, value: float) -> None:
+        self._eps = value
