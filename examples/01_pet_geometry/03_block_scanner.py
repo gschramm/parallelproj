e698709b2e9c581e08c34cf040d2e8b9da067277
Georg Schramm <georg.schramm@kuleuven.be>
2024-06-14 14:29:33 +0200

    work on example

--- examples/01_pet_geometry/03_block_scanner.py
+++ examples/01_pet_geometry/03_block_scanner.py
@@ -1,9 +1,9 @@
-import numpy.array_api as xp
+import array_api_strict as xp
 import parallelproj
 import math
 import matplotlib.pyplot as plt
 
-dev = "cpu"
+dev = None
 
 shape = (3, 2, 2)
 spacing = (1.5, 1.2, 1.7)
@@ -48,18 +48,29 @@ lor_desc = parallelproj.BlockPETLORDescriptor(
     ),
 )
 
-xstart, xend = lor_desc.get_lor_coordinates()
+xstart0, xend0 = lor_desc.get_lor_coordinates(xp.asarray([0], device=dev))
+xstart4, xend4 = lor_desc.get_lor_coordinates(xp.asarray([4], device=dev))
 
 fig = plt.figure(tight_layout=True)
 ax = fig.add_subplot(111, projection="3d")
 scanner.show_lor_endpoints(ax, annotation_fontsize=4, show_linear_index=False)
 
-for i in range(xstart.shape[0]):
+for i in range(xstart0.shape[0]):
     ax.plot(
-        [xstart[i, 0], xend[i, 0]],
-        [xstart[i, 1], xend[i, 1]],
-        [xstart[i, 2], xend[i, 2]],
-        "r-",
-        lw=0.2,
+        [xstart0[i, 0], xend0[i, 0]],
+        [xstart0[i, 1], xend0[i, 1]],
+        [xstart0[i, 2], xend0[i, 2]],
+        color=plt.cm.tab10(0),
+        lw=0.1,
     )
+
+for i in range(xstart0.shape[0]):
+    ax.plot(
+        [xstart4[i, 0], xend4[i, 0]],
+        [xstart4[i, 1], xend4[i, 1]],
+        [xstart4[i, 2], xend4[i, 2]],
+        color=plt.cm.tab10(1),
+        lw=0.1,
+    )
+
 fig.show()
